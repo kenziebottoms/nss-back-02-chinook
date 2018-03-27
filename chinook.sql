acbbefd33acef86664e359cb9b12ff2d57cab91a
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-27 15:45:33 -0500

    1-2 sql queries

--- chinook.sql
+++ chinook.sql
@@ -1,6 +1,14 @@
 -- 1. Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+SELECT
+  FirstName || " " || LastName AS Name,
+  CustomerId,
+  Country
+FROM Customer
+WHERE Country <> "USA";
 
 -- 2. Provide a query only showing the Customers from Brazil.
+SELECT * FROM Customer
+WHERE Country IS "Brazil";
 
 -- 3. Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
 
